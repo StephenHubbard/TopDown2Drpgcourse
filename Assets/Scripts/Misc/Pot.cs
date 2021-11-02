b44675370151e56e88218f6a69923ae0a678b50a
Stephen Hubbard <hubbardgames@gmail.com>
2021-11-02 17:12:00 -0600

    anim updates

---
+++ Assets/Scripts/Misc/Pot.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    [SerializeField] private GameObject blue_rupee;
+
+    
+}
