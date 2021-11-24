2324a55811ff173cc7d75b325f897ee5be61f5d8
Stephen Hubbard <hubbardgames@gmail.com>
2021-11-23 17:34:31 -0700

    clean up

--- Assets/Scripts/Misc/ItemSelection.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemSelection : MonoBehaviour
-{
-    [SerializeField] private GameObject[] itemSlots;
-    [SerializeField] private GameObject selectionBorder;
-}
